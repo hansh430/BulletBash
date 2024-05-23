df2c1e3569f68d80f36348d1260db58b0453aea2
LAPTOP-140RNLEU\Hansu kumar <hanshukumar8459@gmail.com>
2024-05-23 22:47:58 +0530

    -Minor fixes

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -23,6 +23,10 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         ScoreSubject.Instance.UpdateHighScore();
     }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void QuitGame()
     {
         Application.Quit();
